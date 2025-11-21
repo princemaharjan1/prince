a40bdf0021fff47989e964616f642353c7d1ae3c
prince <princehuhu6@gmail.com>
2025-11-21 21:21:04 +0545

    ol

---
+++ senarioforworkshop3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class senarioforworkshop3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class senarioforworkshop3
+{
+    public static void main(String[] args){
+        int fee;
+        float calc;
+        float dev;
+        
+        System.out.println("your fees");
+        Scanner scan=new Scanner(System.in);
+        fee = scan.nextInt();
+        dev=fee*10/100;
+        calc=(fee>=100)?fee-dev:fee-5;
+        System.out.println("your total fee is : " + calc);
+        
+        
+        
+        
+        
+        
+    }
+}
