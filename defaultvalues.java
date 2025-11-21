fbec11ced0eff7773f32545d31bc160a11403c24
prince <princehuhu6@gmail.com>
2025-11-21 14:08:46 +0545

    new

---
+++ defaultvalues.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class defaultvalues here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class defaultvalues
+{
+     byte bit1;
+        short short1;
+        int int1;
+        long long1;
+        
+        float float1;
+        double double1;
+        char char1;
+        String string1;
+        public static void main(String[] args){
+            defaultvalues b1= new defaultvalues();
+       System.out.println(b1.bit1);
+       System.out.println(b1.short1);
+       System.out.println(b1.int1);
+       System.out.println(b1.long1);
+       System.out.println(b1.float1);
+       System.out.println(b1.double1);
+       System.out.println(b1.char1);
+       System.out.println(b1.string1);
+       
+
+       
+    }
+}
