126adefa350fbd6460116c26770cb16d5cd0a4e2
prince <princehuhu6@gmail.com>
2025-11-21 13:49:04 +0545

    workshop second of 3rfd week

---
+++ wroshop_3_ko_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class wroshop_3_ko_2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class wroshop_3_ko_2
+{
+    public static void main(String[] args){
+        System.out.println("please enter a number to determine you paas or fail");
+       Scanner scan = new Scanner(System.in);
+       int first=scan.nextInt();
+       String guess=(first>=40)?"you pass":"your fail";
+       
+       
+       
+       
+        
+    }
+}
