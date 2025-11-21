17a17a1f94a5a3efb0993da9915cbd5d51934a5e
prince <princehuhu6@gmail.com>
2025-11-21 14:14:39 +0545

    rgrg

---
+++ LiteralPractice.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class LiteralPractice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LiteralPractice
+{
+    public static void main(String[] args){
+        long lonog1= 1000000l;
+        float float1=10000.0000f;
+        String cp="\u00a9";
+        System.out.println(float1);
+        System.out.println(lonog1);
+        System.out.println(cp);
+        
+        
+    }
+}
