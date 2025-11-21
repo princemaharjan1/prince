a04adb5128ea689310ab875cf69a1a452888593d
prince <princehuhu6@gmail.com>
2025-11-21 13:58:45 +0545

    princ

---
+++ datatypeinspector.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class workshop_3ko3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class datatypeinspector
+{
+    public static void main(String[] args){
+        byte bit1;
+        short short1;
+        int int1;
+        long long1;
+        
+        float float1;
+        double double1;
+        char char1;
+        String string1;
+        //
+        bit1=5;
+        short1=120;
+        int1=255;
+        long1=254555;
+        float1=2.009909f;
+        double1=2.0020388328;
+        char1='S';
+        string1="srings";
+        
+        
+        
+        
+        
+        
+        System.out.println(bit1);
+        System.out.println(short1);
+        System.out.println(int1);
+        System.out.println(long1);
+        System.out.println(float1);
+        System.out.println(double1);
+        System.out.println(bit1);
+        
+        
+    }
+}
