52d99ef8f2abfc0c04a6d3cb609efa407d8d176e
prince <princehuhu6@gmail.com>
2025-12-12 12:39:46 +0545

    okok

---
+++ tutorialko6.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class tutorialko6 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class tutorialko6
+{
+    public static void main(String[] args){
+        int[] age;
+        age = new int[6];
+        age[0]=0;
+        age[1]=1;
+        age[2]=2;
+        age[3]=3;
+        age[4]=4;
+        age[5]=5;
+        int i;
+         System.out.println(age.length + " : ");
+        for(i = age.length-1;i >=0;i--){
+        
+        System.out.println(age[i]);
+    }
+      int[] age1 = {10,20,30,40,50};
+      System.out.println(age1.length-1 + " : ");
+      
+      
+      //
+      //new one
+      int[][][] volume;
+      volume = new int[4][4][4];
+      
+      
+    }
+}
